22926cd7bbe3776ed15723f8e59988f9abeece6d
SirWhatMatters <150294689+SirWhatMatters@users.noreply.github.com>
2023-11-09 16:17:52 -0600

    Update GetData.py

--- GetData.py
+++ GetData.py
@@ -23,14 +23,25 @@ elif sys.platform.startswith("darwin"):
 else:
     dwf = cdll.LoadLibrary("libdwf.so")
 
-Num_Samples = 16384
-hdwf = c_int()
-sts = c_byte()
+Num_Samples = 50000
+#hdwf = c_int()
+#sts = c_byte()
 rgdSamples1 = (c_double*Num_Samples)()
 rgdSamples2 = (c_double*Num_Samples)()
 Total = numpy.zeros(Num_Samples)
 N_run = 30
 Sample_Rate = 40000.0
+
+hdwf = c_int()
+sts = c_byte()
+hzAcq = c_double(50000)
+nSamples = 50000
+rgdSamples = (c_double*nSamples)()
+cAvailable = c_int()
+cLost = c_int()
+cCorrupted = c_int()
+fLost = 0
+fCorrupted = 0
 
 
 version = create_string_buffer(16)
@@ -50,23 +61,34 @@ if hdwf.value == hdwfNone.value:
 
 
 #set up acquisition
-dwf.FDwfAnalogInFrequencySet(hdwf, c_double(Sample_Rate))
+'''dwf.FDwfAnalogInFrequencySet(hdwf, c_double(Sample_Rate))
 dwf.FDwfAnalogInBufferSizeSet(hdwf, c_int(Num_Samples)) 
 dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(0))
 dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(1))
 #dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(0), c_double(5))
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(1), c_double(5))'''
+
+dwf.FDwfAnalogInFrequencySet(hdwf, hzAcq)
+dwf.FDwfAnalogInBufferSizeSet(hdwf, c_int(nSamples))
+dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(0))
+dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(1))
 dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(1), c_double(5))
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(0), c_double(5))
+dwf.FDwfAnalogInAcquisitionModeSet(hdwf, acqmodeRecord)
+dwf.FDwfAnalogInRecordLengthSet(hdwf, c_double(nSamples/hzAcq.value))
 
 #set up trigger
 dwf.FDwfAnalogInTriggerAutoTimeoutSet(hdwf, c_double(0)) #disable auto trigger
 dwf.FDwfAnalogInTriggerSourceSet(hdwf, trigsrcDetectorAnalogIn) #one of the analog in channels
 #dwf.FDwfAnalogInTriggerSourceSet(hdwf, trigsrcExternal1)
 
-dwf.FDwfAnalogInTriggerTypeSet(hdwf, trigtypeEdge)
-dwf.FDwfAnalogInTriggerChannelSet(hdwf, c_int(0)) # first channel
-dwf.FDwfAnalogInTriggerLevelSet(hdwf, c_double(0.5)) # 0.5V
-#dwf.FDwfAnalogInTriggerConditionSet(hdwf, DwfTriggerSlopeEither)
+#dwf.FDwfAnalogInTriggerSourceSet(hdwf, trigsrcExternal1) 
 dwf.FDwfAnalogInTriggerConditionSet(hdwf, DwfTriggerSlopeRise)
+#dwf.FDwfAnalogInTriggerTypeSet(hdwf, trigtypeEdge)
+dwf.FDwfAnalogInTriggerChannelSet(hdwf, c_int(0)) # first channel
+dwf.FDwfAnalogInTriggerLevelSet(hdwf, c_double(1)) # 0.5V
+#dwf.FDwfAnalogInTriggerConditionSet(hdwf, DwfTriggerSlopeEither)
+#dwf.FDwfAnalogInTriggerConditionSet(hdwf, DwfTriggerSlopeRise)
 
 # or use trigger from other instruments or external trigger
 #dwf.FDwfAnalogInTriggerSourceSet(hdwf, trigsrcExternal1) 
@@ -78,7 +100,7 @@ time.sleep(2)
 print("Starting repeated acquisitions")
 dwf.FDwfAnalogInConfigure(hdwf, c_int(0), c_int(1))
 
-for iTrigger in range(N_run):
+'''for iTrigger in range(N_run):
     # new acquisition is started automatically after done state 
 
     while True:
@@ -107,16 +129,47 @@ for iTrigger in range(N_run):
     us = math.floor(ns/1e3)
     ns -= us*1e3
     ns = math.floor(ns)
-    print(time.strftime("%Y-%m-%d %H:%M:%S", s)+"."+str(ms).zfill(3)+"."+str(us).zfill(3)+"."+str(ns).zfill(3))
+    print(time.strftime("%Y-%m-%d %H:%M:%S", s)+"."+str(ms).zfill(3)+"."+str(us).zfill(3)+"."+str(ns).zfill(3))'''
+
+cSamples = 0
+
+while cSamples < nSamples:
+    dwf.FDwfAnalogInStatus(hdwf, c_int(1), byref(sts))
+    if cSamples == 0 and (sts == DwfStateConfig or sts == DwfStatePrefill or sts == DwfStateArmed) :
+        # Acquisition not yet started.
+        continue
+
+    dwf.FDwfAnalogInStatusRecord(hdwf, byref(cAvailable), byref(cLost), byref(cCorrupted))
     
-dwf.FDwfAnalogOutConfigure(hdwf, c_int(0), c_int(0))
+    cSamples += cLost.value
+
+    if cLost.value :
+        fLost = 1
+    if cCorrupted.value :
+        fCorrupted = 1
+
+    if cAvailable.value==0 :
+        continue
+
+    if cSamples+cAvailable.value > nSamples :
+        cAvailable = c_int(nSamples-cSamples)
+    
+    dwf.FDwfAnalogInStatusData(hdwf, c_int(1), byref(rgdSamples, sizeof(c_double)*cSamples), cAvailable) # get channel 1 data
+    #dwf.FDwfAnalogInStatusData(hdwf, c_int(1), byref(rgdSamples, sizeof(c_double)*cSamples), cAvailable) # get channel 2 data
+    cSamples += cAvailable.value
+
+    
+
 dwf.FDwfDeviceCloseAll()
+
+fSamples = numpy.fft.fft(rgdSamples)
 
 #plt.plot(rgdSamples1)
 
-plt.show()
-plt.plot(rgdSamples2)
-plt.plot(Total/N_run)
+#plt.show()
+freq=numpy.linspace(0,50000,50000)
+plt.plot(freq,numpy.abs(fSamples))
+#plt.plot(Total/N_run)
 plt.show()
 # plt.plot(numpy.fromiter(rgdSamples, dtype = numpy.float))
 # plt.show()
