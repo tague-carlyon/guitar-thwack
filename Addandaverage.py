2a91682081f3a31d8d6c6ff0c56f521e4ef43edc
SirWhatMatters <150294689+SirWhatMatters@users.noreply.github.com>
2023-11-16 15:31:23 -0600

    Update Addandaverage.py

--- Addandaverage.py
+++ Addandaverage.py
@@ -6,31 +6,38 @@ ReadFile = input("Enter file Name: ")
 print(type(ReadFile))
 print(ReadFile)
 
-DataArr = numpy.loadtxt(f"C:\\Reserch Test\{ReadFile}")
-print(DataArr[:,1])
-print(DataArr)
+DataArr = numpy.loadtxt(ReadFile)
+#print(DataArr[:,1])
+#print(DataArr)
 DataTot = 0.0
 time = 0.0
 
 j = len(DataArr)
 
-DataArr = numpy.empty(shape=(j,2))
+#DataArr = numpy.empty(shape=(j,2))
 
 for i in range(j):
     time = DataArr[i,0]
-    print(time)
+#    print(time)
     DataTemp = DataArr[i,1]
-    print(DataTemp)
+#    print(DataTemp)
     DataTot = DataTemp + DataTot
 
-print(DataArr)
+#print(DataArr)
 
 DataAdjust = DataTot / j
 
 for k in range(j):
     DataArr[k,1] = DataArr[k,1] - DataAdjust
 
-print(DataArr)
+#print(DataArr)
 
-plt.plot(DataArr[:,0],DataArr[:,1])
+SaveFile = input('Enter save file name: ')
+SFile = open(SaveFile, 'w')
+
+for l in range(j):
+    SFile.write(str(DataArr[l,0])+' '+str(DataArr[l,1]))
+
+Farr =numpy.fft.fft(DataArr[:,1])
+plt.plot(DataArr[:,0],numpy.abs(Farr))
 plt.show()
